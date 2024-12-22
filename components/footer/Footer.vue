bc04a7bc8c669e94040458466a8000549426d456
vxrpenter <david.reulen@web.de>
2024-12-22 12:39:11 +0100

    footer change

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -2,29 +2,29 @@
 </script>
 
 <template>
-  <div>
+  <div data-aos="fade-down">
     <footer class="px-4 py-4 max-w-screen-xl mx-auto">
-      <UContainer >
+      <UContainer>
         <UDivider class="py-4 gap-2"/>
       </UContainer>
       <UContainer class="flex py-4 gap-2 lg:gap-52">
-        <div>
+        <div data-aos="zoom-in">
           <h2>Getting Started</h2>
           <p><NuxtLink to="/regelwerk">Lies die Regeln</NuxtLink></p>
         </div>
-        <div>
+        <div data-aos="zoom-in">
           <h2>Community</h2>
           <p><NuxtLink to="https://discord.gg/3uGrFYr98n">Discord</NuxtLink></p>
           <p><NuxtLink to="https://www.tiktok.com/@ravenblacksl?_t=8qXazWhGLYX&_r=1">TikTok</NuxtLink></p>
           <p><NuxtLink to="https://www.instagram.com/ravenblacksl/profilecard/">Instagram</NuxtLink></p>
           <p><NuxtLink to="https://youtube.com/@ravenblacksl?feature=shared">Youtube</NuxtLink></p>
         </div>
-        <div>
+        <div data-aos="zoom-in">
           <h2>Ravenblack</h2>
           <p><NuxtLink to="/ueber-uns">Über Uns</NuxtLink></p>
           <p><NuxtLink to="/team">Unser Team</NuxtLink></p>
         </div>
-        <div>
+        <div data-aos="zoom-in">
           <h2>Terms</h2>
           <p><NuxtLink to="/impressum">Impressum</NuxtLink></p>
           <p><NuxtLink to="/terms">Terms</NuxtLink></p>
@@ -40,7 +40,7 @@
           <div class="float-left">
             <NuxtLink to="/"><h1 id="align_text">Ravenblack</h1></NuxtLink>
           </div>
-          <div class="float-right">
+          <div data-aos="fade-right" class="float-right">
             <NuxtLink to="/licencing"><p id="align_text">© 2024 Ravenblack</p></NuxtLink>
           </div>
         </UContainer>
