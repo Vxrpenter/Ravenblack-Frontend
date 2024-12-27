e1dfae9382a071a162142fcdaadfce3359de049b
vxrpenter <david.reulen@web.de>
2024-12-27 14:44:00 +0100

    Fix

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -8,24 +8,24 @@
         <UDivider class="py-4 gap-2"/>
       </UContainer>
       <UContainer class="flex py-4 gap-2 lg:gap-52">
-        <div data-aos="zoom-in">
+        <div>
           <h2>Getting Started</h2>
           <p><NuxtLink to="/regelwerk/scp">Lies die SL Regeln</NuxtLink></p>
           <p><NuxtLink to="/servers">Suche dir einen Server aus</NuxtLink></p>
         </div>
-        <div data-aos="zoom-in">
+        <div>
           <h2>Community</h2>
           <p><NuxtLink to="https://discord.gg/3uGrFYr98n">Discord</NuxtLink></p>
           <p><NuxtLink to="https://www.tiktok.com/@ravenblacksl?_t=8qXazWhGLYX&_r=1">TikTok</NuxtLink></p>
           <p><NuxtLink to="https://www.instagram.com/ravenblacksl/profilecard/">Instagram</NuxtLink></p>
           <p><NuxtLink to="https://youtube.com/@ravenblacksl?feature=shared">Youtube</NuxtLink></p>
         </div>
-        <div data-aos="zoom-in">
+        <div>
           <h2>Ravenblack</h2>
           <p><NuxtLink to="/ueber-uns">Über Uns</NuxtLink></p>
           <p><NuxtLink to="/team">Unser Team</NuxtLink></p>
         </div>
-        <div data-aos="zoom-in">
+        <div>
           <h2>Terms</h2>
           <p><NuxtLink to="/impressum">Impressum</NuxtLink></p>
           <p><NuxtLink to="/terms">Terms</NuxtLink></p>
@@ -42,7 +42,7 @@
             <NuxtLink to="/"><h1 id="align_text">Ravenblack</h1></NuxtLink>
           </div>
           <div class="float-right">
-            <NuxtLink to="/licencing"><p id="align_text">© 2024 Ravenblack</p></NuxtLink>
+            <NuxtLink to="https://github.com/Vxrpenter/Ravenblack-Frontend/blob/master/LICENCE"><p id="align_text">© 2024 Ravenblack</p></NuxtLink>
           </div>
         </UContainer>
       </div>
